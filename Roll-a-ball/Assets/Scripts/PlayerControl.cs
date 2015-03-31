3a037d2d831ade452f197349b08d432a65a937c7
Wu Haoxiang <Drk@WudeiMac.local>
2015-03-31 13:15:00 +0800

    add pickups and ontriggerenter  also we can count our pickups when we pick it up

--- Roll-a-ball/Assets/Scripts/PlayerControl.cs
+++ Roll-a-ball/Assets/Scripts/PlayerControl.cs
@@ -2,7 +2,16 @@
 using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
+
 	public float speed;
+	private int count;
+	public GUIText countText;
+
+	void start()
+	{
+		count = 0;
+		SetCountText ();
+	}
 
 	void FixedUpdate()
 	{
@@ -11,5 +20,20 @@ public class PlayerControl : MonoBehaviour {
 
 		Vector3 movement = new Vector3 (moveHorizongtal,0,moveVertical);
 		GetComponent<Rigidbody>().AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "PickUp")
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "count:" + count.ToString ();
 	}
 }
